aba07cb7b8d7085f13b9f080c14cfb2f34d4347d
arseny kaptelinin <arseny.kaptelinin@yandex.ru>
2025-05-19 22:29:58 +0300

    its time to stop

--- generate_dev_conflict.ps1
+++ generate_dev_conflict.ps1
@@ -1,6 +1,6 @@
 <#
-    PowerShell script that creates dev_conflict.txt filled with
-    dummy lines to force merge conflicts.
+    PowerShell script that creates dev_conflict.txt
+    filled with dummy lines to force merge conflicts.
 #>
 
 param(
@@ -8,6 +8,7 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
+
 $UID       = [guid]::NewGuid()
 $Timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
 $Hash      = (git rev-parse HEAD).Trim()
@@ -16,22 +17,23 @@ $Hash      = (git rev-parse HEAD).Trim()
 ############################################################
 #   WARNING: DEVELOPMENT BRANCH WAS RECREATED
 ############################################################
-'@ | Out-File $OutFile -Encoding utf8
+'@ | Out-File -FilePath $OutFile -Encoding utf8
 
-"Branch unique ID : $UID"       | Out-File $OutFile -Append -Encoding utf8
-"Creation date    : $Timestamp" | Out-File $OutFile -Append -Encoding utf8
-"Commit hash      : $Hash"      | Out-File $OutFile -Append -Encoding utf8
-"############################################################" | Out-File $OutFile -Append -Encoding utf8
-"" | Out-File $OutFile -Append -Encoding utf8
+"Branch unique ID : $UID"        | Out-File -FilePath $OutFile -Append -Encoding utf8
+"Creation date    : $Timestamp"  | Out-File -FilePath $OutFile -Append -Encoding utf8
+"Commit hash      : $Hash"       | Out-File -FilePath $OutFile -Append -Encoding utf8
+"############################################################" | Out-File -FilePath $OutFile -Append -Encoding utf8
+"" | Out-File -FilePath $OutFile -Append -Encoding utf8
 
 1..500 | ForEach-Object {
-    "Instruction #$_: update local branch -> git fetch origin && git checkout dev && git pull" |
-        Out-File $OutFile -Append -Encoding utf8
+    "Instruction #$($_) - update local branch -> git fetch origin && git checkout dev && git pull" |
+        Out-File -FilePath $OutFile -Append -Encoding utf8
 }
 
 1..500 | ForEach-Object {
     $Rand = -join ((48..57)+(65..90)+(97..122) | Get-Random -Count 32 | ForEach-Object {[char]$_})
-    "Conflict line #$_: $Rand" | Out-File $OutFile -Append -Encoding utf8
+    "Conflict line #$($_) - $Rand" |
+        Out-File -FilePath $OutFile -Append -Encoding utf8
 }
 
 @'
@@ -39,4 +41,4 @@ $Hash      = (git rev-parse HEAD).Trim()
 #   If you see this file your local branch is outdated!
 #   Please update before pushing.
 ############################################################
-'@ | Out-File $OutFile -Append -Encoding utf8
+'@ | Out-File -FilePath $OutFile -Append -Encoding utf8
